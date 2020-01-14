3763b6f64d65c60b8410a8378937b37c2b54cb35
jgopireddy <jagjeevan.gopireddy@greenskycredit.com>
2020-01-14 16:15:21 -0500

    first commit

---
+++ src/main/java/com/jgopireddy/aws/cicd/AwsEcsCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.jgopireddy.aws.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEcsCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsEcsCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgopireddy/aws/cicd/AwsEcsCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgopireddy.aws.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsEcsCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
